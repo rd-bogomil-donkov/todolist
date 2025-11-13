706f7271d0ed4c637ada870d634ae6e39e96eb26
rd-bogomil-donkov <bogomil.donkov@rewe-group.com>
2025-11-13 11:32:44 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, model, signal } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatInputModule, MatIcon, FormsModule, MatButton, MatListModule, MatCardModule, MatCheckboxModule, MatRadioModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('todolist');
+  @Input() value: string = '';
+  readonly checked = model(false);
+
+  items: string[] = []
+
+  addItem(){
+    if(!this.items.find(i=> i===this.value) && this.value.trim().length>0){
+      this.items.push(this.value)
+    }
+  }
 }
